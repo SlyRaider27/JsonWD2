4c6e877a4e49aa5ef96a639c80ed1ce294d1a8cc
SARTIN <sartinh@otc.edu>
2017-11-01 09:42:31 -0500

    It Prints

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-var _ = require("lodash");
+var _ = require('lodash');
+var jsonfile = require('jsonfile');
+var file = 'data.json';
+
+jsonfile.readFile(file, function(err, obj) {
+    _.forEach(obj, value =>{
+        console.log(value);
+    });
+}); 
